c9bd0f11feb4cd83e457e308d5fc99e73ace4a04
illumir03 <illumir03@gmail.com>
2026-09-11 22:14:45 +0800

    first launch

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const menu = document.querySelector('.menu');
+const links = document.querySelector('.nav-links');
+if(menu){
+  menu.addEventListener('click',()=>{
+    links.classList.toggle('open');
+    menu.setAttribute('aria-expanded', links.classList.contains('open'));
+  });
+}
+document.querySelectorAll('.nav-links a').forEach(a=>{
+  a.addEventListener('click',()=>links?.classList.remove('open'));
+});
